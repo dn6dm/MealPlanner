668f048587aae07a38932d8053d4652f3937d5c1
dn6dm <dn6dm@virginia.edu>
2018-10-25 16:05:56 -0700

    Created home page view

--- planner/views.py
+++ planner/views.py
@@ -1,8 +1,7 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic.base import TemplateView
 
 
 # Create your views here.
 
-def home(request):
-    return render(request, 'planner/base.html')
+class HomePage(TemplateView):
+    template_name = 'planner/home.html'
