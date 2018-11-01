2cc371f85ca29695e1397e2396bae1b00ab0bb67
dn6dm <dn6dm@virginia.edu>
2018-11-01 13:30:35 -0700

    Modified profile model

--- users/models.py
+++ users/models.py
@@ -4,6 +4,9 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    carbs = models.IntegerField()
+    fat = models.IntegerField()
+    protein = models.IntegerField()
 
     def __str__(self):
         return f'{self.user.username} Profile'
