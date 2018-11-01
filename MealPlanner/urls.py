94da70128ce7d31936216f7362ea876de067ad64
dn6dm <dn6dm@virginia.edu>
2018-11-01 14:47:36 -0700

    Reconfigured urls

--- MealPlanner/urls.py
+++ MealPlanner/urls.py
@@ -13,14 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin, auth
+from django.contrib import admin
 from django.urls import path, include
 from users import views as users_views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('planner.urls')),
     path('register/', users_views.user_new, name='user_new'),
-    path('login/', auth.views.login, {'template_name': 'login.html'}, name='login'),
-    path('logout/', auth.views.logout, {'next_page': 'login/'}, name='logout')
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout')
 ]
