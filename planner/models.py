142337facb47142463076f57bc1d8c087a522e36
dn6dm <dn6dm@virginia.edu>
2018-11-02 13:23:16 -0700

    Added food models and forms

--- planner/models.py
+++ planner/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from users.models import Profile
+
 
 # Create your models here.
+class FoodItem(models.Model):
+    name = models.CharField(max_length=50)
+    calories = models.IntegerField(default=0)
+    carbs = models.IntegerField(default=0)
+    fat = models.IntegerField(default=0)
+    protein = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
+
+
+class FoodPlan(models.Model):
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    fooditems = models.ManyToManyField(FoodItem)
+    calories = models.IntegerField(default=0)
+    carbs = models.IntegerField(default=0)
+    fat = models.IntegerField(default=0)
+    protein = models.IntegerField(default=0)
