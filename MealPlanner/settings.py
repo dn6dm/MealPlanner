13196d563238964288c47b59e6df0a319b698b2e
dn6dm <dn6dm@virginia.edu>
2018-11-19 12:54:34 -0800

    Getting env ready for push to heroku

--- MealPlanner/settings.py
+++ MealPlanner/settings.py
@@ -161,3 +161,10 @@ LOGIN_REDIRECT_URL = '/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
